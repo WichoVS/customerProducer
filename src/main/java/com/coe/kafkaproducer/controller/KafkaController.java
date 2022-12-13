4fb5d0911126ac0c4cf11a284045e99cef497a71
WichoVS <luis.villanueva.1029@gmail.com>
2022-12-13 10:14:59 -0600

    CORS CrossOrigin annotation added

--- src/main/java/com/coe/kafkaproducer/controller/KafkaController.java
+++ src/main/java/com/coe/kafkaproducer/controller/KafkaController.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("customer/produce")
+@CrossOrigin
 public class KafkaController {
     @Autowired
     private KafkaTemplate<String, Customer> kafkaUserTemplate;
